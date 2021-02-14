dda1230f875f565e3660ffde63ce4a72ed293295
sawan <sawansaini4life@gmail.com>
2021-02-15 01:54:12 +0530

    successfully creating react app using cra

--- actions/init.js
+++ actions/init.js
@@ -1,23 +1,34 @@
 const loader = require("../lib/loader");
 
-const { exec } = require("child_process");
+const { spawn } = require("child_process");
 
 module.exports = function()
 {
-
   const creatingReactAppLoader = loader();
 
-  creatingReactAppLoader.text = "Creating React App ...";
+  creatingReactAppLoader.text = "Initiating React App ...";
   creatingReactAppLoader.start();
 
-  /* exec("npx create-react-app myapp", function(err, stdout, stderr)
-  {
-    console.log(err);
-    console.log(stdout);
-    console.log(stderr);
-  }) */
-
   
+  const reactAppCommand = spawn("npx",["create-react-app","my_app"]);
+ 
+  reactAppCommand.stdout.on("data", data => {
+    console.log(`${data}`);
+  });
+
+  reactAppCommand.stderr.on("data", data => {
+    console.log(`${data}`);    
+    creatingReactAppLoader.stop();
+  });
+
+  reactAppCommand.on('error', (error) => {
+      console.log(error);
+      creatingReactAppLoader.fail(error.message);
+  });
+
+  reactAppCommand.on("close", code => {
+      creatingReactAppLoader.succeed("Creation of React App Succeeded");
+  });
   
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
@@ -7,22 +9,33 @@ const inquirer = require('./lib/inquirer');
 
 const setupMVVM = require('./actions/setup_mvvm');
 const init = require('./actions/init');
+const NumberPrompt = require('inquirer/lib/prompts/number');
+const { combineReducers } = require('@reduxjs/toolkit');
 
 
 console.log(
   chalk.yellow(
-    figlet.textSync('FarMart MVVM', { horizontalLayout: 'full' })
+    figlet.textSync('R-Arch', { horizontalLayout: 'full' })
   )
 );
 
 async function main()
 {
+  
+  var args = process.argv.slice(2);
+
+  if(args.length)
+  {
+    // if arguments are provided we will call suitable file
+    //directly, else user have to choose from list
+    return 
+  }
+
   let action_result = await inquirer.startup();
 
   if( action_result.action === "init" )
   {
     init();
-    //setupMVVM();
   }
   else 
   {
@@ -31,3 +44,11 @@ async function main()
 }
 
 main();
+
+
+todo
+1. create function should use yarn or npm
+2. install react-router-dom and redux and react-redux
+3. creating basic folders
+4. create route
+5. create model
