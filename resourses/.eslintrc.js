b2da24e877729e322d5c44a953afa53fe730c086
nikhil <nikhil@farmart.co>
2021-03-17 15:13:17 +0530

    setting eslintrc with absolutre imports

--- resourses/.eslintrc.js
+++ resourses/.eslintrc.js
@@ -3,24 +3,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: {
-        paths: ['src'],
-        alias: {
-          "assets": "./src/assets",
-          "apis": "./src/api",
-          "atoms": "./src/components/atoms",
-          "molecules": "./src/components/molecules",
-          "organisms": "./src/components/organisms",
-          "components": "./src/components",
-          "navigations": "./src/navigations",
-          "screens": "./src/screens",
-          "containers": "./src/containers",
-          "models": "./src/models",
-          "view_models": "./src/view_models",
-          "services": "./src/services",
-          "styles": "./src/styles",
-          "utils": "./src/utils",
-          "store": "./src/store",
-          "constants": "./src/constants",
+          moduleDirectory: ["node_modules", "src/"]
         },
       },
     },
