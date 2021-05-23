7f96cf1bbafec59d882047e10fb53a71f35e1cc2
nikhil <nikhil@farmart.co>
2021-05-23 23:41:22 +0530

    bug fixed : added double quote in roite template text key

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
 const startup_choices_values  = {
   init_new_project: "init_new_project",
   create_new_screen: "create_new_screen",
-  create_new_route: "create_new_route"
+  create_new_model: "create_new_model"
 }
 
 const startup_choices = [
@@ -14,8 +14,8 @@ const startup_choices = [
     value:startup_choices_values.create_new_screen,
   }, 
   {
-    name:"Create Route",
-    value:startup_choices_values.create_new_route
+    name:"Create New Model",
+    value:startup_choices_values.create_new_model
   }
 ]
 
